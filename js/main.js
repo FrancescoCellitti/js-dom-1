638862c6914fdcab7e94e7ccfeb4feb1db72dbe1
FrancescoCellitti <francescocellitti@virgilio.it>
2025-06-05 15:19:01 +0200

    another method for accendispengi

--- js/main.js
+++ js/main.js
@@ -7,8 +7,13 @@ bottoneEl.addEventListener('click', accendi);
 function accendi() {
     imgEl.classList.toggle('d-none');
     img2El.classList.toggle('d-none');
-    document.querySelector('span').classList.toggle('d-none')
-    document.querySelector('p').classList.toggle('d-none')
+    if (img2El.classList.contains('d-none')){
+        bottoneEl.textContent='Accendi';
+    }else{
+        bottoneEl.textContent='spegni'
+    }
+    /* document.querySelector('span').classList.toggle('d-none')
+    document.querySelector('p').classList.toggle('d-none') */
     
 }
 
